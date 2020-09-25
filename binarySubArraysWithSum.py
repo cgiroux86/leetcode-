c9509a0a9e87f30541f1e3da598bb47615d3690b
Chris Giroux <chris.giroux86@gmail.com>
2020-09-25 17:07:17 -0400

    num teams complete using dp

--- binarySubArraysWithSum.py
+++ binarySubArraysWithSum.py
@@ -1,18 +1,67 @@
 
-def numSubarraysWithSum(A, S):
-    res = 0
-    sub = 0
-    for i in range(len(A)):
-        sub = A[i]
-        if sub == S:
-            res += 1
-        for j in range(i + 1, len(A)):
-            sub = sub + A[j]
-            if sub == S:
-                res += 1
-            if sub > S:
-                break
-    return res
+def countSubarrays(arr, n, k):
+
+    start = 0
+    end = 0
+    count = 0
+    sum = arr[0]
+
+    while (start < n):
+
+        # If sum is less than k, move end
+        # by one position. Update count and
+        # sum accordingly.
+        if (sum == k):
+            count += 1
+            sum += arr[end]
+            end += 1
+
+        elif sum < k:
+            end += 1
+            # For last element, end may become
+            sum += arr[end]
+
+        # If sum is greater than or equal to k,
+        # subtract arr[start] from sum and
+        # decrease sliding window by moving
+        # start by one position
+
+        else:
+            sum -= arr[start]
+            start += 1
+
+    return count
 
 
-numSubarraysWithSum([0, 0, 0, 0, 0], 0)
+# Driver Code
+array = [1, 0, 1, 0, 1]
+k = 2
+size = len(array)
+count = countSubarrays(array, size, k)
+print(count)
+# def numSubarraysWithSum(A, S):
+#     curr_max = A[0]
+#     total_max = 0
+#     res = 0
+
+#     for i in range(len(A)):
+#         if curr_max == S:
+#             res += 1
+#         curr_max = max(curr_max, curr_max + A[i])
+#     print(res)
+# res = 0
+# sub = 0
+# for i in range(len(A)):
+#     sub = A[i]
+#     if sub == S:
+#         res += 1
+#     for j in range(i + 1, len(A)):
+#         sub = sub + A[j]
+#         if sub == S:
+#             res += 1
+#         if sub > S:
+#             break
+# return res
+
+
+# numSubarraysWithSum([0, 0, 0, 0, 0], 0)
---
+++ numsTeams.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numTeams(self, rating):
+        n = len(rating)
+
+        up = [0] * n
+        down = [0] * n
+
+        teams = 0
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i + 1, n):
+                if rating[i] < rating[j]:
+                    up[i] += 1
+                    teams += up[j]
+                else:
+                    down[i] += 1
+                    teams += down[j]
+
+        return teams
+
+
+s = Solution()
+
+print(s.numTeams([2, 5, 3, 4, 1]))
